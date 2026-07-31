9ee6be9f58b345427a3c344910e08c0bce2759dd
tenpsn-10 <i.pongsatorn2544@gmail.com>
2026-07-31 11:11:40 +0700

    up

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       script: "server.js",
       cwd: "./backend", // ชี้เข้าไปในโฟลเดอร์ backend
       instances: 1,
+      exec_mode: "fork",
       autorestart: true,
       watch: false,
       max_memory_restart: "300M",
